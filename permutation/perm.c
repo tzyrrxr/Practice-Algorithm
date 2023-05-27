55944686c2d91804649182074ce68ecde2ffa63c
tzyrrxr <sjlin0827@outlook.com>
2023-05-27 18:01:20 +0800

    2023-05-27 permutation

--- permutation/perm.c
+++ permutation/perm.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
-void perm() {
+//#define SWAP(A, B, TMP) ((TMP)=(A), (A)=(B), (B)=(TMP))
+void SWAP (int *a, int *b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void perm(int Arr[], int index, int ArrSize) {
+	// edge condition
+	if (index == ArrSize-1) {
+		// printf result
+		for (int j=0; j<ArrSize; j++) {
+			if (j != ArrSize-1) {
+				printf("%d, ", Arr[j]);
+			} else {
+				printf("%d\n", Arr[j]);
+			}
+		}
+	} else {
+		for (int i=index; i < ArrSize; i++) {
+			// Arr[index] ~ Arr[ArrSize] swap into Arr[index] 
+			SWAP (Arr+index, Arr+i);
+			// change into the next permutation
+			perm (Arr, index+1, ArrSize);
+			// Recovery order.
+			SWAP (Arr+index, Arr+i); 
+		}
+	}
+	
 }
 
 int main(void) {
+	int Arr[] = {10, 20, 30, 40};
+	perm (Arr, 0, 3);
 	return 0;
 }
